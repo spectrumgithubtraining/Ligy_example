c175552cb20744f1b0e6d24e339da2d1f41f2081
Spectrum Training <150805887+spectrumgithubtraining@users.noreply.github.com>
2023-12-19 09:26:30 +0530

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,12 +1,22 @@
-def simpleGeneratorFun(): 
-    yield 1
-    yield 2
-    yield 3
-   
-# x is a generator object 
-x = simpleGeneratorFun() 
-  
-# Iterating over the generator object using next 
-print(x.next()); 
-print(x.next()); 
-print(x.next()); 
+# def simpleGeneratorFun():
+#     yield 1
+#     yield 2
+#     yield 3
+
+# # x is a generator object
+# x = simpleGeneratorFun()
+
+# # Iterating over the generator object using next
+# print(next(x))
+# print(next(x))
+# print(next(x))
+
+def square_generator(n):
+    for i in range(n):
+        yield i ** 2
+# Using the generator
+n_values = 5
+squares = square_generator(n_values)
+# Iterating over the generator
+for square in squares:
+    print(square)
